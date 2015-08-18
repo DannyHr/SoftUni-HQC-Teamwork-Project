f5dfb23223f7d2ddc3a9df029f942e978344a2ad
donstany <donstany79@gmail.com>
2015-08-18 13:24:07 +0300

    Documentation of class HttpPostParameter

--- RestSharp/RestSharp/Authenticators/OAuth/HttpPostParameter.cs
+++ RestSharp/RestSharp/Authenticators/OAuth/HttpPostParameter.cs
@@ -4,21 +4,49 @@ namespace RestSharp.Authenticators.OAuth
 
     internal class HttpPostParameter : WebParameter
     {
+        /// <summary>
+        /// Initialize a new instance of the HttpPostParameter class
+        /// </summary>
+        /// <param name="name">The name of HttpPostParameter</param>
+        /// <param name="value">The value of HttpPostParameter</param>
         public HttpPostParameter(string name, string value)
             : base(name, value)
         {
         }
 
+        /// <summary>
+        /// Gets the httpPostParameter Type 
+        /// </summary>
         public virtual HttpPostParameterType Type { get; private set; }
-
+     
+        /// <summary>
+        /// Gets the filename
+        /// </summary>
         public virtual string FileName { get; private set; }
 
+        /// <summary>
+        /// Gets the filePath
+        /// </summary>
         public virtual string FilePath { get; private set; }
 
+        /// <summary>
+        /// Gets the fileStream 
+        /// </summary>
         public virtual Stream FileStream { get; set; }
 
+        /// <summary>
+        /// Gets the ContentType 
+        /// </summary>
         public virtual string ContentType { get; private set; }
 
+        /// <summary>
+        /// Initialize a static HttpPostParameter class
+        /// </summary>
+        /// <param name="name">The name of HttpPostParameter</param>
+        /// <param name="fileName">The fileName of HttpPostParameter</param>
+        /// <param name="filePath">The filePath of HttpPostParameter</param>
+        /// <param name="contentType">The contentType of HttpPostParameter</param>
+        /// <returns>The contentType of HttpPostParameter object</returns>
         public static HttpPostParameter CreateFile(string name, string fileName, string filePath, string contentType)
         {
             var parameter = new HttpPostParameter(name, string.Empty)
@@ -31,6 +59,13 @@ namespace RestSharp.Authenticators.OAuth
             return parameter;
         }
 
+        /// <summary>
+        /// Initialize a static HttpPostParameter class
+        /// </summary>
+        /// <param name="name">The name of HttpPostParameter</param>
+        /// <param name="fileName">The fileName of HttpPostParameter</param>
+        /// <param name="contentType">The contentType of HttpPostParameter</param>
+        /// <returns>The contentType of HttpPostParameter object</returns>
         public static HttpPostParameter CreateFile(string name, string fileName, Stream fileStream, string contentType)
         {
             var parameter = new HttpPostParameter(name, string.Empty)

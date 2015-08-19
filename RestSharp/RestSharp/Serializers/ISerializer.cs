c1436006334309d7ae92bd92ce8c0fb1e73d1fc9
donstany <donstany79@gmail.com>
2015-08-20 00:19:59 +0300

    fix white space in interface ISerializer

--- RestSharp/RestSharp/Serializers/ISerializer.cs
+++ RestSharp/RestSharp/Serializers/ISerializer.cs
@@ -23,30 +23,26 @@ namespace RestSharp.Serializers
         /// </summary>
         /// <param name="obj">Object to serialize.</param>
         /// <returns></returns>
-        
         string Serialize(object obj);
 
        /// <summary>
         /// Gets or sets the RootElement.
        /// </summary>
-       
-        string RootElement { get; set; }
+       string RootElement { get; set; }
 
         /// <summary>
         /// Gets or sets the Namespace
         /// </summary>
-        
         string Namespace { get; set; }
 
         /// <summary>
         /// Gets or sets the DateFormat
         /// </summary>
         string DateFormat { get; set; }
-
+        
         /// <summary>
         /// Gets or sets the ContentType
         /// </summary>
-        
         string ContentType { get; set; }
     }
 }

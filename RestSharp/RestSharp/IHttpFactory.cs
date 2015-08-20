1e9a833dc9cfa65ce50e88e72c9262f9fc058d6b
DannyHr <dani_a1@ymail.com>
2015-08-20 12:48:25 +0300

    Documented IHttpFactory.cs

--- RestSharp/RestSharp/IHttpFactory.cs
+++ RestSharp/RestSharp/IHttpFactory.cs
@@ -1,10 +1,17 @@
 ﻿namespace RestSharp
 {
+    /// <summary>
+    /// Interface for creating new request
+    /// </summary>
     public interface IHttpFactory
     {
         IHttp Create();
     }
 
+    /// <summary>
+    /// Factory for creating new request
+    /// </summary>
+    /// <typeparam name="T">type of request</typeparam>
     public class SimpleFactory<T> : IHttpFactory 
         where T : IHttp, new()
     {
